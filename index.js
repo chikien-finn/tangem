daab3e87f2c4d0fe0e783944d05add7fbdeb5c66
chikien-finn <anhtxsc000@gmail.com>
2024-06-03 21:20:19 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+// nút có
+document.getElementById('bam').addEventListener('click', function(){
+    alert('em đang buồn à, vậy thì nhắn cho anh đi, anh vẫn đang chờ đây')
+})
+
+// nút không 
+document.getElementById('bam1').addEventListener('click', function(){
+    alert('không buồn à, vậy thì nhắn cho anh đi, anh thấy em không ổn')
+})
